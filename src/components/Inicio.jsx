5d0be7d3c22fc1af3e5eb8adbc2f06831b5d7447
BrianCamunez <camunezbenitezbrian@fpllefia.com>
2025-01-14 18:15:06 +0100

    Tarea 3: Crea componente para la vista Juego

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -7,7 +7,7 @@ const Inicio = () => {
         <div className="card-body">
           <h1 className="card-title text-center">Instrucciones de Tetris</h1>
           <p className="card-text">
-            Tetris es un juego clásico en el que el objetivo es encajar piezas de diferentes formas (llamadas "tetrominos") para formar líneas horizontales completas. Cuando una línea se completa, desaparece y el jugador gana puntos. El juego continúa hasta que las piezas se apilan hasta la parte superior de la pantalla.
+            Tetris es un juego clásico en el que el objetivo es encajar piezas de diferentes formas (llamadas tetrominos) para formar líneas horizontales completas. Cuando una línea se completa, desaparece y el jugador gana puntos. El juego continúa hasta que las piezas se apilan hasta la parte superior de la pantalla.
           </p>
           
           <img className="img-fluid mx-auto d-block" src="https://m.media-amazon.com/images/I/61M3rDwh4qL.png" alt="Foto Tetris" />
@@ -21,7 +21,7 @@ const Inicio = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Inicio;
+export default Inicio
---
+++ src/components/Juego.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Juego = () =>{
+    return(
+        <div className="container mt-5">
+            <h2 className="text-center border border-primary rounded p-3 mt-4 mb-4 bg-light">Aqui va el juego</h2>
+    </div>
+    )
+}
+
+export default Juego
