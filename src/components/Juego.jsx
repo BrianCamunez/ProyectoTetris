a93b9bcb57dbd9cff57473ac8e83a85a0ae32743
BrianCamunez <camunezbenitezbrian@fpllefia.com>
2025-01-14 20:19:44 +0100

    Tarea 2: Componente panel.jsx

--- src/components/Juego.jsx
+++ src/components/Juego.jsx
@@ -1,9 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Panel from "./Panel"
+import { useState } from 'react';
+import modelos from "../lib/modelos";
 const Juego = () =>{
+        const [arrayCasillas, setArrayCasillas] = useState(modelos.matriz)
     return(
         <div className="container mt-5">
             <h2 className="text-center border border-primary rounded p-3 mt-4 mb-4 bg-light">Aqui va el juego</h2>
+            <Panel matriz={arrayCasillas}/>
     </div>
     )
 }
---
+++ src/components/Panel.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Panel = ({ matriz }) => {
+  // Estilos en línea para el panel
+  const panelStyle = {
+    display: 'grid',
+    gridTemplateRows: 'repeat(22, 20px)',  // 22 filas de 20px
+    gridTemplateColumns: 'repeat(12, 20px)',  // 12 columnas de 20px
+    gap: '0px', // Eliminamos el gap para que las celdas se toquen
+    backgroundColor: '#222',
+  };
+
+  const celdaStyle = {
+    width: '20px',
+    height: '20px',
+    backgroundColor: '#fff',
+    border: '1px solid #222',  // Borde para que se vea claramente
+  };
+
+  const celdaOcupadaStyle = {
+    ...celdaStyle,
+    backgroundColor: '#054', // Color para las celdas ocupadas
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+      <div style={panelStyle}>
+        {matriz.map((fila, indexFila) => (
+          // Cada fila debe contener 12 celdas
+          fila.map((celda, indexColumna) => (
+            <div
+              key={indexFila * 12 + indexColumna}  // Clave única para cada celda
+              style={celda !== 0 ? celdaOcupadaStyle : celdaStyle}
+            />
+          ))
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Panel;
