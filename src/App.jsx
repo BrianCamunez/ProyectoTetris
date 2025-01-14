d88c6d0a9d7b6674b033ba749f99c2232a3246d2
BrianCamunez <camunezbenitezbrian@fpllefia.com>
2025-01-14 19:18:00 +0100

    Tarea 7: Añade menú de navegación

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import Inicio from "./components/Inicio";
 import Partidas from "./components/Partidas";
 import Juego from "./components/Juego";
 import Ranking from "./components/Ranking";
+import Header from "./components/Header";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/Partidas" element={<Partidas />} />
-        <Route path="/Juego" element={<Juego />} />
-        <Route path="/Ranking" element={<Ranking/>}/>
-      </Routes>
+      <Header />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/Partidas" element={<Partidas />} />
+          <Route path="/Juego" element={<Juego />} />
+          <Route path="/Ranking" element={<Ranking />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
