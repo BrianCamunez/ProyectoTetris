89c5dbb895b85894d9f9a2c70aa7e8c1f0ed0a79
BrianCamunez <camunezbenitezbrian@fpllefia.com>
2025-01-14 18:29:38 +0100

    Tarea 6: Configura rutas con React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
-import Partidas from "./views/Partidas";
+import Inicio from "./components/Inicio";
+import Partidas from "./components/Partidas";
+import Juego from "./components/Juego";
+import Ranking from "./components/Ranking";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Partidas />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/Partidas" element={<Partidas />} />
+        <Route path="/Juego" element={<Juego />} />
+        <Route path="/Ranking" element={<Ranking/>}/>
+      </Routes>
+    </Router>
   );
 }
 
