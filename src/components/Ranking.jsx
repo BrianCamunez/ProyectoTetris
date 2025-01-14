fc9ba0a915755d45d0f4ba74a97bfc45abc7d97c
BrianCamunez <camunezbenitezbrian@fpllefia.com>
2025-01-14 18:21:40 +0100

    Tarea 5: Crea componente para la vista Ranking

---
+++ src/components/Ranking.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Ranking = () =>{
+    return(
+        <div className="container mt-5">
+            <h2 className="text-center border border-primary rounded p-3 mt-4 mb-4 bg-light">Aqui va el ranking</h2>
+    </div>
+    )
+}
+
+export default Ranking
